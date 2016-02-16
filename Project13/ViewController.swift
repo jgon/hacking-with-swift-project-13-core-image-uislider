bc13a44f05924875e0fbd090ff42659b57326b62
JackG <jacques.gonzalez@me.com>
2016-02-16 18:04:44 +0100

    Added Core Image sepia filter.

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var currentImage: UIImage!
     
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "YACIFP"
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        context = CIContext(options: nil)
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +38,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     @IBAction func intensityChanged(sender: UISlider) {
+        applyProcessing()
     }
     
     @IBAction func importPicture(sender: UIBarButtonItem) {
@@ -56,10 +62,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismissViewControllerAnimated(true, completion: nil)
         
         currentImage = newImage
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func applyProcessing() {
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        
+        let cgImage = context.createCGImage(currentFilter.outputImage!, fromRect: currentFilter.outputImage!.extent)
+        let processedImage = UIImage(CGImage: cgImage)
+        imageView.image = processedImage
     }
 }
 
